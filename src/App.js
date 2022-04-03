635ab97a9bfbd5bfd99595ba476a074d4b9fd476
zobaerahmedzihad1 <zobaerahmedzihad02@gmail.com>
2022-04-03 06:39:27 +0600

    Basic create context and state setup done.

--- src/App.js
+++ src/App.js
@@ -4,26 +4,33 @@ import SecondComponent from "./Components/SecondComponent/SecondComponent";
 import ThirdComponent from "./Components/ThirdComponent/ThirdComponent";
 import { GrFormAdd } from "react-icons/gr";
 import { AiOutlineMinus } from "react-icons/ai";
+import { createContext, useState } from "react";
+
+export const CountContext = createContext();
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="app">
-      <div className="counter-container">
-        <h1>App</h1>
-        <div>
-          <button title="Increase">
-            <GrFormAdd size={20} />
-          </button>
-          <h1>Count: </h1>
-          <button title="Decrease">
-            <AiOutlineMinus size={20} />
-          </button>
+    <CountContext.Provider>
+      <div className="app">
+        <div className="counter-container">
+          <h1>App</h1>
+          <div>
+            <button title="Increase" onClick={() => setCount(count + 1)}>
+              <GrFormAdd size={20} />
+            </button>
+            <h1>Count: {count} </h1>
+            <button title="Decrease" onClick={() => setCount(count - 1)}>
+              <AiOutlineMinus size={20} />
+            </button>
+          </div>
         </div>
+        <FirstComponent />
+        <SecondComponent />
+        <ThirdComponent />
       </div>
-      <FirstComponent />
-      <SecondComponent />
-      <ThirdComponent />
-    </div>
+    </CountContext.Provider>
   );
 }
 
